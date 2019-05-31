e29631d1bab957c4d19b7f4f4307a32d67510e50
Tony Le <letony@icloud.com>
2019-05-31 16:26:28 +1000

    Implemented gun offset debug system

---
+++ Assets/Scripts/AdjustGunOffset.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustGunOffset : MonoBehaviour
+{
+    [SerializeField] Toggle useLocalPosition;
+    [SerializeField] Text textX;
+    [SerializeField] Text textY;
+    [SerializeField] Text textZ;
+
+    [SerializeField] Slider sliderX;
+    [SerializeField] Slider sliderY;
+    [SerializeField] Slider sliderZ;
+
+    [SerializeField] GameObject gun;
+
+    
+    Vector3 offset;
+
+    void Update()
+    {
+        SetOffset();
+        UpdateText();
+    }
+
+    private void SetOffset()
+    {
+        //Constantly set the gun's offset
+        offset.x = sliderX.value;
+        offset.y = sliderY.value;
+        offset.z = sliderZ.value;
+
+        if (useLocalPosition.isOn)
+            gun.transform.localPosition = offset;
+        else
+            gun.transform.position = offset;
+    }
+
+    private void UpdateText()
+    {
+        //Update the GUI
+        textX.text = offset.x.ToString();
+        textY.text = offset.y.ToString();
+        textZ.text = offset.z.ToString();
+    }
+
+    public void Reset()
+    {
+        offset = new Vector3();
+    }
+
+}
---
+++ Assets/Scripts/GunOffsetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunOffsetter : MonoBehaviour
+{
+    Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    public void SetX(float x)
+    {
+        pos.x = x;
+    }
+    public void SetY(float y)
+    {
+        pos.y = y;
+    }
+    public void SetZ(float z)
+    {
+        pos.z = z;
+    }
+
+
+}
