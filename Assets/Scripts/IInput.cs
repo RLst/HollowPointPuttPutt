86479347b037225188e996d749a2e9007c492001
Tony Le <letony@icloud.com>
2019-06-05 17:09:40 +1000

    Basic input controller implemented

---
+++ Assets/Scripts/IInput.cs
@@ -0,0 +1,25 @@
+﻿namespace HollowPoint
+{
+public interface IInput
+{
+    //Direction
+    UnityEngine.Quaternion rotation { get; }
+
+    //Touchpad
+    UnityEngine.Vector2 axis { get; }
+
+    bool touch { get; }
+    bool touched { get; }
+
+    bool click { get; }
+    bool clicked { get; }
+
+    //Trigger
+    bool fire { get; }
+    bool fired { get; }
+
+    //Back button
+    bool back { get; }
+    bool wasBack { get; }
+}
+}
---
+++ Assets/Scripts/OGoInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+namespace HollowPoint
+{
+    public class OGoInput : MonoBehaviour, IInput
+    {
+        //Direction
+        public Quaternion rotation => OVRInput.
+
+        //Touchpad axis
+        public Vector2 axis => OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+        //Touchpad click
+        public bool click => OVRInput.Get(OVRInput.Button.One);
+        public bool clicked => OVRInput.GetDown(OVRInput.Button.One);
+
+        //Touchpad touch
+        public bool touch => OVRInput.Get(OVRInput.Touch.One);
+        public bool touched => OVRInput.GetDown(OVRInput.Touch.One);
+
+
+        //Trigger
+        public bool fire => OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+
+        public bool fired => OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+
+        public bool back => OVRInput.Get(OVRInput.Button.Two);
+
+        public bool wasBack => OVRInput.GetDown(OVRInput.Button.Two);
+
+        void FixedUpdate()
+        {
+            OVRInput.FixedUpdate(); //Must be run each fixed frame
+        }
+
+        void Update()
+        {
+            OVRInput.Update();  //Must be run each frame
+        }
+    }
+}
