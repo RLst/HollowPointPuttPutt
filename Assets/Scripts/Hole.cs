503b04afdcf67fdca9162596c263c6e015f87bfd
Andrew Kenny <andrewckenny97@gmail.com>
2019-06-07 16:29:25 +1000

    Added sound effects and hole hit detection

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HollowPoint
+{
+    [RequireComponent(typeof(CapsuleCollider))]
+    public class Hole : MonoBehaviour
+    {
+        [SerializeField] UnityEvent OnSink;
+ 
+        void OnTriggerEnter(Collider col)
+        {
+            OnSink.Invoke();
+            Destroy(col.gameObject);
+        }
+    }
+}
