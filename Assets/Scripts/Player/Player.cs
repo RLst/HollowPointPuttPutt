34e5262104a93b6521fa583ae358ccfcde7f17ea
Tony Le <letony@icloud.com>
2019-06-07 13:09:31 +1000

    Added basic aiming trajectory line

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,54 +1,10 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace HollowPoint
 {
-    //Player controller
     public class Player : MonoBehaviour
     {
-        [SerializeField] float lookSensitivity = 150f;
-        [SerializeField] Transform cam;
-        IInput input;
-        Gun gun;
-
-
-        void Awake()
-        {
-            LockCursor();
-        }
-
-        void Start()
-        {
-            input = GetComponent<IInput>();
-            gun = GetComponentInChildren<Gun>();
-        }
-
-        void Update()
-        {
-            HandleShooting();
-        }
-
-        void HandleShooting()
-        {
-            if (input.fired)
-            {
-                gun.Shoot();
-            }
-        }
-
-        void CameraRotation()
-        {
-            
-            transform.Rotate(transform.right * );
-        }
-
-
-
-        void LockCursor()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
 
     }
 }
---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+namespace HollowPoint
+{
+    public class PlayerAim : MonoBehaviour
+    {
+        [SerializeField] LineRenderer trajectoryLine;
+        [SerializeField] float lineLength = 5f;
+        Gun gun;
+
+        void Awake()
+        {
+            gun = GetComponentInChildren<Gun>();
+        }
+
+        void Start()
+        {
+            //Line renderer uses world space
+            trajectoryLine.useWorldSpace = true;
+        }
+
+        void Update()
+        {
+            DrawTrajectoryLine();
+        }
+
+        void DrawTrajectoryLine()
+        {
+            //If the gun is aiming within the bounds of the ball
+            if (gun.Raycast<Ball>(out Ball ball, out RaycastHit hitInfo))
+            {
+                //Get normalized trajectory vector
+                var trajectory = (ball.transform.position - hitInfo.point).normalized;
+
+                //Draw trajectory line from the center of the ball
+                Vector3[] linePoints = { ball.transform.position, ball.transform.position + trajectory * lineLength };
+                trajectoryLine.SetPositions(linePoints);
+            }
+            //Otherwise don't set the trajectory line
+            else {
+                Vector3[] linePoints = { transform.position + transform.forward * 1f - transform.right * 1f, transform.position + transform.forward * 1f + transform.right * 1f };
+                trajectoryLine.SetPositions(linePoints);
+            }
+        }
+
+    }
+}
